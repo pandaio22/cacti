8aedfd19aeb614499a9765ab87940323ca8956b9
Rodolfo Carapau <rodolfo.carapau@gmail.com>
2025-06-30 14:34:19 +0100

    feat(satp-hermes): rebased to satp-stg
    Signed-off-by: Rodolfo Carapau <rodolfo.carapau@gmail.com>

--- packages/cacti-asset-schema-architecture-plugin-satp/src/test/typescript/integration/entities/asset-provider/asset-provider-token-issuance-authorization-request.test.ts
+++ packages/cacti-asset-schema-architecture-plugin-satp/src/test/typescript/integration/entities/asset-provider/asset-provider-token-issuance-authorization-request.test.ts
@@ -1,1 +1,6 @@
-describe.skip("Asset Provider Token Issuance Authorization Request Test", () => {});
+describe.skip("Asset Provider Token Issuance Authorization Request Test", () => {
+  it.skip("Test", () => {
+    // This test is skipped, as indicated by the .skip method.
+    // You can implement the test logic here when needed.
+  });
+});
